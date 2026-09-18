d9d644ca90a425469b91284d5e3be60942dbb0c1
Tanu Chatrey <tanuchatrey0201@gmail.com>
2026-09-18 16:43:06 +0530

    Create linked.py

---
+++ linked.py
@@ -0,0 +1,48 @@
+# Node class
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+# Linked List class
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert at the end
+    def insert(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+
+        temp.next = new_node
+
+    # Display the linked list
+    def display(self):
+        temp = self.head
+
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+
+        print("None")
+
+
+# Create linked list
+ll = LinkedList()
+
+# Insert elements
+ll.insert(10)
+ll.insert(20)
+ll.insert(30)
+ll.insert(40)
+
+# Display
+ll.display()
